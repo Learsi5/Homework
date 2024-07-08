c3ebf1bf6c679be3c0de4d0fce34d1ba24e1cf0c
Learsi5 <lewislearsi5@gmail.com>
2024-07-08 12:19:17 -0500

    wk2homework.js

---
+++ wk2homework.js
@@ -0,0 +1,22 @@
+// In your Homework repo, create a new directory named "Week2" and then within that directory, create a JavaScript file - name it something that indicates it is homework for Week 2
+// Inside your JS file, create two variables: pizzaPlace and numberOfToppings
+// Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers.
+const pizzaplace = "Learsi Pizza";
+
+let numberOfToppings = 5;
+
+console.log(pizzaplace, typeof pizzaPlace); // string
+console.log(numberOfToppings, typeof numberOfToppings); // number
+
+console.log(`At ${pizzaplace} we offer ${numberOfToppings} Pizza Toppings.`);
+
+if (numberOfToppings <= 10) {
+  console.log("Quality, not quantity.");
+}
+
+
+// Print the variables and their types.
+// Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
+// Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
+// Add, commit, and push your JS file to your GitHub repo.
+// WHEN FINISHED: Send your TA a message to let them know it's complete and make sure that your Jira ticket for homework submission includes a link to your GitHub Homework.
